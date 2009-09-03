6d7e729a8dcd614433a2f5601c0380f162f6540f
mathematix <tianweisun@gmail.com>
2009-09-03 15:09:57 +0800

    update 1

---
+++ src/com/ics/tcg/web/user/ureg/client/Reg_Service.java
@@ -0,0 +1,15 @@
+package com.ics.tcg.web.user.ureg.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("reg")
+public interface Reg_Service extends RemoteService {
+	
+	/**save a friend*/
+	String saveUser(User_Client user);
+	
+	
+	/**check if is a friend*/
+	Integer check(String username);
+}
